31ad677b63e837423cfbc96664da7d39ce926d03
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-17 15:54:49 -0700

    0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - this program determine whether the input number is positive or negative
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+	if (n==o)
+			{
+				printf(n "is zero");
+			}
+	else 
+		{
+			if (n > 0)
+				{
+					printf(n "is positive");
+				}
+			else 
+				{
+					printf(n "is negative");
+				}
+
+		}
+	return (0);
+}
+
