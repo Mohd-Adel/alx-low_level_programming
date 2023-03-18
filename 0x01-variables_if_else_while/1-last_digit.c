db8866b981284668863ed54c97fb3b55abb362d4
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-17 17:05:16 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,20 +16,20 @@ n = rand() - RAND_MAX / 2;
 m = n % 10;
 if (n > 5)
 {
-x = "and is greater than 5\0"
+x = "and is greater than 5\0";
 }
 else
 {
 if (x == 0)
 {
-x = "and is 0\0"
+x = "and is 0\0";
 }
 else
 {
-x = "and is less than 6 and not 0\0"
+x = "and is less than 6 and not 0\0";
 }
 }
-printf("Last digit of %d is %d %s\n", n, m, x)
+printf("Last digit of %d is %d %s\n", n, m, x);
 return (0);
 }
 
