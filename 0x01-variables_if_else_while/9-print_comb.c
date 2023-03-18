82256aa2576abfed45c422f8529ad4a0e8a3683a
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-17 17:39:09 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this program prints possible combinations of single-digit numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
+
