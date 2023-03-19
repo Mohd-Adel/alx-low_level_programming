4e5b7a04208204a359907e1fa4901b8b166d34fc
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-19 08:21:14 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This program print all possible combinations of 2 digits
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+	for (i;0<=i<=9;i++)
+	{
+		for(j;0<=j<=9;j++)
+		{
+			putchar( ij,)
+		}
+
+	}
+	return (0)
+}
