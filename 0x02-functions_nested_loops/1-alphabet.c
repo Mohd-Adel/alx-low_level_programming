d70d2ba7bcbe3b0a524ab87f23cb4f4581857198
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-21 11:17:19 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main- prints the alphabet, in lowercase, followed by a new line.
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef main.h
+#define main.h>
+void print_alphabet(void)
+{
+	int i;
+	for (i = 97; i <= 122; i++)	
+	{
+		putchar(i);
+		if (i == z)
+		{
+			putchar(10);
+		}
+	}
+};
+#endif
