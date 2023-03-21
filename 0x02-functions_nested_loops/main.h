629d9497bda4e488ee74c37065f16d708d0f8841
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-21 15:34:48 -0700

    2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,10 +2,38 @@
 #define _MAIN
 /**
  *print_alphabet - prints alphabets
- _putchar - prints _putchar
+ *_putchar - prints _putchar
+ *print_alphabet_x10 -
+ *_islower -
+ *_isalpha -
+ *print_sign -
+ *_abs -
+ *print_last_digit -
+ *jack_bauer -
+ *times_table -
 */
 void _putchar(char c);
 
 void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
 
 #endif
