c5ba13a2c5f0427888d04899f22bef90f9513980
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-20 19:30:41 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - this program prints possible combinations of two two-digit numbers
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+int i, j, k, l;
+for (i = 48; i <= 57; i++)
+{
+	for (j = 48; j <= 57; j++)
+	{
+		for (k = 48; k <= 57; k++)
+		{
+			for (l = 48; l <= 57; l++)
+			{
+			if (k >= i && l >= j)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(' ');
+				putchar(k);
+				putchar(l);
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		}
+	}
+}
+}
