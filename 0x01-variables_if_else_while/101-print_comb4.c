45b541b0a8207b9c4e33cc403143dbe30d0c8b68
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-20 18:37:18 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - this program prints possible different combinations of three digits.
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+int i, j, k;
+for (i = 48 ; i <= 57 ; i++)
+{
+	for (j = 49 ; j <= 56 ; j++)
+	{
+		for (k = 50 ; k <= 55 ; k++)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(k);
+			putchar(",");
+			putchar(" ");
+
+		}
+	}
+}
+return (0);
+}
+
