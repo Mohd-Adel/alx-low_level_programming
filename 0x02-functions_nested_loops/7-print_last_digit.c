2ea70227ed6a784fc6ede073f36f2c362b177f87
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-21 18:02:36 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - checks for lowercase character.
+ * Return: 0 (Success)
+ * @n: input
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	x = (n % 10) + 48;
+	_putchar(x);
+}
+
