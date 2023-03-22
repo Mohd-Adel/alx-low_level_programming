d9d5593ff42bf64e2b6ac05e76389e211c88dc60
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-21 17:40:53 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,14 +8,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar(43);
 		return (1);
 	}
 	if (n < 0)
 	{
+		_putchar(45);
 		return (-1);
 	}
 	if (n == 0)
 	{
+		_putchar(48);
 		return (0);
 	}
 }
