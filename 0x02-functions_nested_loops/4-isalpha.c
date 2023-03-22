9d73670f6d6c38da298796d6971cb4902858bd1a
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-21 17:17:12 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - checks for lowercase character.
+ * Return: 0 (Success)
+ * @c: letter to be checked
+ */
+int _isalpha(int c)
+{
+	return (isalpha(c));
+}
