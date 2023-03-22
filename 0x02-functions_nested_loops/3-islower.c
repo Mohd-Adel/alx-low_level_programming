25f8f6a55dfbc95d10e4fe6209618c584a91d0fc
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-21 17:08:42 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,11 @@
 #include <ctype.h>
 /**
  * _islower - checks for lowercase character.
- * c : letter to be checked
+ * Return: 0 (Success)
+ * @c: letter to be checked
  */
 int _islower(int c)
 {
-	islower(c);
+	return (islower(c));
 }
 
