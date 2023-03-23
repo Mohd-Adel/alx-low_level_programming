899e3dbb5e181247a67bd36ed90f126bd4221091
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-22 17:45:59 -0700

    100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_times_table - prints the n times table, starting with 0.
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			_putchar("0" + (i * j);
+			_putchar("0" + ",");
+			_putchar("0" + " ");
+			_putchar("0" + " ");
+			_putchar("0" + " ");
+		}
+	}
+}
