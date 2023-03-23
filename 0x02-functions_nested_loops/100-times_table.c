4ac791085f0b7a8d9a87f8504938370a3cb3aa4b
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-22 18:15:05 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,18 +13,18 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			_putchar('0' + (i * j));
+			_putchar('0' + ((i * j) % 10));
 			if (j < n)
 			{
 			_putchar(',');
 			_putchar(' ');
-			if ((i * j) < 100)
+			if ((i * j) >= 100)
 			{
-			_putchar(' ');
+			_putchar('0' + ((i * j) / 100));
 			}
-			if ((i * j) < 10)
+			if ((i * j) >= 10)
 			{
-			_putchar(' ');
+			_putchar('0' + ((i * j) / 10) );
 			}
 			}
 		}
