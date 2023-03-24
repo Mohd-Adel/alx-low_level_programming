aea0499aa7a29a241d33191b7489d73b45907d54
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-23 18:31:19 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,6 +11,8 @@ void print_line(int n)
 	{
 	_putchar('\n')
 	}
+	else
+	{
 	for (i = 1; i <= n; i++)
 	{
 		_putchar(95);
@@ -18,5 +20,6 @@ void print_line(int n)
 		{
 			_putchar('\n');
 		}
+	}
 	}
 }
