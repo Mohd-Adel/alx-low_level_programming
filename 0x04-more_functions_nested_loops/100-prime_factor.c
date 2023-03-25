7e3b97439c0af7671329eafa4c0217ddf9e9cfe3
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-24 17:08:59 -0700

    100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints the largest prime factor of the number.
+ * Return: 0 or 1.
+ */
+int main(void)
+{
+	int n;
+
+	while ((n % 5) == 0 || (n % 3) == 0 || (n % 2) == 0)
+	{
+		if ((n % 2) == 0)
+		{
+			n = n / 2;
+		}
+		else if ((n % 3) == 0)
+		{
+			n = n / 3;
+		}
+		else if ((n % 5) == 0)
+		{
+			n = n / 5;
+		}
+		else
+		{
+			return (n);
+		}
+	}
+}
