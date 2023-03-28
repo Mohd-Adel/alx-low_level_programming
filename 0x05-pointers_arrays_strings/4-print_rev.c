a307ff8de97dacff74e01d362dc9a5f2fc215178
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-28 03:32:17 +0200

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,10 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = strlen(s) ; i > 0; i--)
+	for (i = strlen(s) - 1 ; i >= 0; i--)
 	{
 	_putchar(s[i]);
-	if (i == 1)
+	if (i == 0)
 	{
 		_putchar(10);
 	}
