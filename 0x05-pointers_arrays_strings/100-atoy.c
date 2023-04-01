14f5a86286d610da206651c4b8c273f2d557eafd
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-03-31 21:17:35 -0700

    100

--- 0x05-pointers_arrays_strings/100-atoy.c
+++ 0x05-pointers_arrays_strings/100-atoy.c
@@ -7,14 +7,17 @@
  */
 int _atoi(char *s)
 {
-	int i, j;
+	int i, j = 0, opt;
 	int len = strlen(s);
 
 	for (i = 0; i <= len; i++)
 	{
 		if (s[i] <= 9 && s[i] >= 0)
 		{
-			_putchar(s[i] + '0');
+			opt[j] = (s[i] + '0');
+			j++;
 		}
 	}
+
+	return (opt);
 }
