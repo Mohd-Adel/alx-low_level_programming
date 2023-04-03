4786e6e3f04616dcc47f0ab85155c4d3ad40614a
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-04-03 21:56:10 +0200

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring.
+ * @haystack: input
+ * @needle: input
+ * Return: 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *j = needle;
+
+		while (*i == *j && *j != '\0')
+		{
+			i++;
+			j++;
+		}
+
+		if (*j == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
