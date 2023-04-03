3a478117406b16803f4a8c07d856a0d6497a5d55
root <root@Do0ola.localdomain>
2023-04-03 02:25:35 +0200

    100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ *rot13 -  encodes a string using rot13.
+ */
+
+char *rot13(char *input)
+{
+	int i, j = 0;
+	char output;
+
+	for (i = 0; i <= strlen(input); i++)
+	{
+		if (input[i] < n)
+		{
+			output[j] = input[i] + 13;
+			j++;
+		}
+		else
+		{
+			output[j] = input[i] - 13;
+			j++
+		}
+	}
+	return (output);
+}
