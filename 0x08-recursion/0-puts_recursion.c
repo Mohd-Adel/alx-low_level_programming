ad3dda780395e79edffc6b25ce59c2f81aac651a
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-04-05 21:11:01 +0200

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,13 @@ unsigned int i = 0;
 void _puts_recursion(char *s)
 {
 	_putchar(s[i]);
+	if (i == strlen(s))
+	{
+		_putchar('\n');
+	}
 	i++;
 	if (i < strlen(s))
 	{
 	_puts_recursion(s);
 	}
-	
 }
-_putchar('\n');
