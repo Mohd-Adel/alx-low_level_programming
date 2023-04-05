69d69cbfe71ad7e304f9a531d790993f05032e8b
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-04-05 20:42:35 +0200

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+*_puts_recursion -  prints a string, followed by a new line.
+*@s: input string.
+*/
+void _puts_recursion(char *s)
+{
+	int i = 0, j;
+
+	_putchar(s[i]);
+	_putchar('\n');
+	i++;
+	if (i < strlen(s)
+	{
+	_puts_recursion(*s);
+	}
+}
