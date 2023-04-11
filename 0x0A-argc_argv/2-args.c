df96daee397bd2db3e64d6f76c82c0b7f645da60
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-04-11 04:54:27 +0200

    1

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives.
+ * @argc: numb
+ * @argv: array of arguments.
+ * Return: 0 - (success).
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
