7001fe1994332d7555e0b0b588561cb46dae09a4
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-04-11 04:49:50 +0200

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: Num
+ * @argv: name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
