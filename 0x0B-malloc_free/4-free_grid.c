a695b501dcf67b47db45fd799c9aff80be45d536
Mohd-Adel <mohamed.adel.ism@outlook.com>
2023-04-12 04:39:22 +0200

    3

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * free_grid - frees a 2 dimensional grid previously created 
+ * free_grid - frees a 2 dimensional grid previously created
  * @grid: input grid
  * @height: input height
  */
