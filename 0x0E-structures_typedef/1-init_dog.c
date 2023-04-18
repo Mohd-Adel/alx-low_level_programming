84c33cb6e62aa30c2b9bc97d37d980f715cb58fb
root <root@Do0ola.localdomain>
2023-04-18 11:29:54 +0200

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ *init_dog - function that initializes a new dog
+ *@d: struct dog type
+ *@name: input dog name
+ *@age: input dog age.
+ *@owner: input dog owner.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog d
+	{
+		dog.name = name;
+		dog.age = age;
+		dog.owner = owner;
+	};
+}
