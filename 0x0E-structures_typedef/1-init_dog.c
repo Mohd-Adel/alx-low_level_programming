cd934380dc5b08054d3ab777d075d506548f1538
root <root@Do0ola.localdomain>
2023-04-18 11:32:19 +0200

    1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,10 +8,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog d
-	{
-		dog.name = name;
-		dog.age = age;
-		dog.owner = owner;
-	};
+	struct dog d;
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
 }
