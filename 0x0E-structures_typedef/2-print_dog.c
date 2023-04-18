2090f6324a7fac53cde92fe70a60815a2179347a
root <root@Do0ola.localdomain>
2023-04-18 13:05:12 +0200

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,15 +14,15 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-			printf("Name: %s\n", d->name);
+			printf("Name: %s\n", (d->name));
 		}
-		if (d->age == 0)
+		if ((d->age) == 0)
 		{
 		printf("Age: (nil)\n");
 		}
 		else
 		{
-		printf("Age: %f\n", d->age);
+		printf("Age: %f\n", (d->age));
 		}
 		if (!(d->owner))
 		{
@@ -30,7 +30,7 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-		printf("Owner: %s\n", d->owner);
+		printf("Owner: %s\n", (d->owner));
 		}
 	}
 }
