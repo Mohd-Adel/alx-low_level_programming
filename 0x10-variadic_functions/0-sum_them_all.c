539d718df8909324fe1353a15d6a583b4632c02d
root <root@Do0ola.localdomain>
2023-04-21 04:01:23 +0200

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,10 +1,9 @@
 #include "variadic_functions.h"
 /**
- *  * sum_them_all - returns the sum of all its parameters.
- *   * @n: amount of the arguments.
- *    *
- *     * Return: sum of its parameters.
- *      */
+ *sum_them_all - returns the sum of all its parameters.
+ *@n: input arguments.
+ *Return: sum of parameters.
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
