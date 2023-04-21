a6b91b2efa15c2571b97323f6b09cdfdb6dc5222
root <root@Do0ola.localdomain>
2023-04-21 04:03:46 +0200

    0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,9 @@
 #ifndef variadic_functions
 #define variadic_functions
+
+#include <stdarg.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
