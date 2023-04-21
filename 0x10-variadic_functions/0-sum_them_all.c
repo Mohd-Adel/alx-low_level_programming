b5b2f7307d27163b51e8e2c2ccf5da546c808622
root <root@Do0ola.localdomain>
2023-04-21 04:02:50 +0200

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,12 +15,10 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 	}
 	va_start(valist, n);
-
 	for (i = 0; i < n; i++)
 	{
 	sum += va_arg(valist, int);
 	}
 	va_end(valist);
-
 	return (sum);
 }
