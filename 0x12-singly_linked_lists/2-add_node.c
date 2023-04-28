aab6f4505b02b9c9127ffb3d2d1dbcf6b4780fd7
root <root@Do0ola.localdomain>
2023-04-28 04:16:11 +0200

    2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ *add_node - adds a new node at the beginning of a list_t list.
+ *@head: pointer to the head.
+ *@str:input string
+ *Return: returns the address to the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *cpy;
+	int length;
+	list_t *elem;
+
+	elem = malloc(sizeof(list_t));
+	if (elem == NULL)
+	{
+	return (NULL);
+	}
+	cpy = strdup(str);
+	if (cpy == NULL)
+	{
+	free(elem);
+	return (NULL);
+	}
+	for (length = 0; str[length];)
+	{
+	len++;
+	}
+	elem->str = cpy;
+	elem->len = len;
+	elem->next = *head;
+
+	*head = elem;
+
+	return (elem);
+
+}
