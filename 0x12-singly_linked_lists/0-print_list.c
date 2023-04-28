d1ef2582a8dca718380ee62e190a48f26dda1dc1
root <root@Do0ola.localdomain>
2023-04-28 03:47:41 +0200

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d] %s\n", h->len ? h->len : 0, h->str ? h->str : "(nil)");
+		printf("[%d] %s\n", h->str ? h->len : 0, h->str ? h->str : "(nil)");
 		j++;
 	}
 	return (j);
