6198c7e02c3bbb49c998d317730cc7b449a37736
root <root@Do0ola.localdomain>
2023-04-28 04:02:19 +0200

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,6 @@
 #include "lists.h"
 
 /**
- *_strlen - returns the input length.
- *@c: input.
- *Return: length of input
- */
-int _strlen(char *c)
-{
-	int i = 0;
-
-	if (!c)
-	{
-	return (0);
-	}
-	while (*c++)
-	{
-	i++;
-	}
-	return (i);
-}
-
-/**
  *print_list - prints all the elements of a list_t list.
  *@h: input list.
  *Return: Number of nodes.
@@ -28,12 +8,13 @@ int _strlen(char *c)
 
 size_t print_list(const list_t *h)
 {
+	const list_t *ptr = h;
 	size_t j = 0;
 
-	while (h)
+	while (ptr != NULL)
 	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
-		h = h->next;
+		printf("[%d] %s\n", ptr->str ? ptr->len : 0, ptr->str ? ptr->str : "(nil)");
+		ptr = ptr->next;
 		j++;
 	}
 	return (j);
