0917498c34229941c4409145ee7deabd8dd6a166
root <root@Do0ola.localdomain>
2023-04-28 04:36:10 +0200

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,6 @@ void __attribute__((constructor)) before_main(void);
  */
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
