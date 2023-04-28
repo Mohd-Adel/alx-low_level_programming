13efeb1f8394c691cc5f02823d61c48ed23e0cb0
root <root@Do0ola.localdomain>
2023-04-28 13:55:30 +0200

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,11 +25,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (i = 0; i <= strlen(s1); i++)
+	for (i = 0; i < strlen(s1); i++)
 	{
 		ptr[i] = s1[i];
 	}
-	for (j = i; j <= i + n; j++)
+	for (j = i; j < i + n; j++)
 	{
 		ptr[j] = s2[k];
 		k++;
