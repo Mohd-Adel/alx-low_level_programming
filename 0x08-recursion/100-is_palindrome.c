78974bed052d9ec35e1812d19ba990fcd47f74cd
root <root@Do0ola.localdomain>
2023-04-28 09:32:46 +0200

    100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+/**
+ * is_palindrome - returns 1 if a string is a palindrome
+ * @s: input string.
+ * Return: 1 if success.
+ */
+
+int helperfunction (char *input , int start , int end )
+{
+	if (*(input) != *(input + end - 1))
+	{
+	return (0);
+	}
+	if (*(input) == '/0')
+	{
+	return (1);
+	}
+	return ( helperfunction(*input, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome
+ * @s: input string.
+ * Return: 1 if success.
+ */
+int is_palindrome(char *s)
+{
+	if (!*s)
+	{
+	return (1);
+	}
+	return (helperfunction(*s, 0, strlen(s)));
+}
