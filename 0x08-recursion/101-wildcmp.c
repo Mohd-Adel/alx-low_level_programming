6bf686f3236b21d6a5aa3d2921fad11aaa3295c4
root <root@Do0ola.localdomain>
2023-04-28 11:18:46 +0200

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,14 +8,6 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2)
-	{
-	return (wildcmp(s1 + 1, s2 + 1));
-	}
-	if (*s2 == '*')
-	{
-	return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-	}
 	if (*s2 == '\0' && *s1 == '\0')
 	{
 	return (1);
@@ -24,5 +16,15 @@ int wildcmp(char *s1, char *s2)
 	{
 	return (0);
 	}
+	if (*s1 == *s2)
+	{
+	return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	if (*s2 == '*')
+	{
+	return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+
 	return (0);
 }
