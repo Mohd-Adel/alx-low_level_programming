6217ced83a070543eabdbc74c3200f542f8c2f34
root <root@Do0ola.localdomain>
2023-04-29 06:09:21 +0200

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
+/**
+ * clac_len - function to calculate length.
+ * @str: input string.
+ * Return: str len.
+ */
+
+int calc_len(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0';)
+	{
+		i++;
+	}
+	return (i);
+}
 
 /**
  * string_nconcat - concatenates two strings.
@@ -12,29 +27,44 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, k;
+	unsigned int lens1, lens2, i, j;
 	char *ptr;
 
-	k = 0;
-	if (n >= strlen(s2))
+	if (s1 == NULL)
 	{
-		n = strlen(s2);
+		lens1 = 0;
 	}
-	ptr = malloc((strlen(s1) * sizeof(char)) + (n * sizeof(char)) + 1);
+	else
+	{
+		lens1 = calc_len(s1);
+	}
+	if (s2 == NULL)
+	{
+		lens2 = 0;
+	}
+	else
+	{
+		if (n >= calc_len(s2))
+		{
+			lens2 = calc_len(s2);
+		}
+		else
+		{
+			lens2 = n;
+		}}
+	ptr = malloc((lens1 + lens2 + 1) * sizeof(char));
 	if (!ptr)
 	{
 		return (NULL);
 	}
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < lens1; i++)
 	{
 		ptr[i] = s1[i];
 	}
-	for (j = i ; j < i + n ; j++)
+	for (j = 0 ; j < lens2; j++)
 	{
-		ptr[j] = s2[k];
-		k++;
+		ptr[j + lens1] = s2[j];
 	}
-	ptr[strlen(s1) + strlen(s2)] = '\0';
+	ptr[lens1 + lens2] = '\0';
 	return (ptr);
-
 }
