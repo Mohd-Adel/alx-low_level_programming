dfe22dd32e72553fc222d8c0feb73a96dfa472f7
root <root@Do0ola.localdomain>
2023-04-29 21:17:23 +0200

    2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: list head address.
+ * @n: n value.
+ * Return: adress of new element.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	const listint_t *new;
+
+	if (head)
+	{
+		new->next = head;
+		new->n = n;
+		return (&new);
+	}
+	return (NULL);
+}
