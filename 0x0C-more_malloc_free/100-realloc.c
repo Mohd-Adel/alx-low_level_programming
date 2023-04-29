0d668d3d5ed48987e01e93fd04a77c331716d926
root <root@Do0ola.localdomain>
2023-04-29 13:48:03 +0200

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -46,7 +46,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0)
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+	else if (new_size == 0)
 	{
 		if (ptr)
 			free(ptr);
