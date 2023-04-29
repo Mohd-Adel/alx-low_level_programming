8b74c1b19944f1708054e9fd7041bcedb6ab35a9
root <root@Do0ola.localdomain>
2023-04-29 14:07:20 +0200

    101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * main - multiplies two positive numbers.
+ * @argc: args number.
+ * @argv: args values.
+ * Return: multiple of numbers.
+ */
+
+void main(int argc, char *argv[])
+{
+	unsigned long result;
+	int i, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+
+	result = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", result);
+}
