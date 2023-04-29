234a538ea6f3e80fd322720590e7f74aad1377a6
root <root@Do0ola.localdomain>
2023-04-29 12:14:40 +0200

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
-	for (i = 0; i < (nmemb * size); i++)
+	for (i = 0; ptr[i]; i++)
 	{
 		ptr[i] = 0;
 	}
