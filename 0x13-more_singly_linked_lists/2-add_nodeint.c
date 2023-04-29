d45b597c21d6a01320ac8871c29451b46416d8ce
root <root@Do0ola.localdomain>
2023-04-29 21:50:48 +0200

    2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,11 +13,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *item;
 
 	item = malloc(sizeof(listint_t));
-	if (head)
+	if (!head)
 	{
-		item->next = *head;
-		item->n = n;
-		return (item);
+		return (NULL);
 	}
-	return (NULL);
+	item->next = *head;
+	item->n = n;
+	return (item);
 }
