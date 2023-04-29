47a1aaab50e06c421cdeb5b7c526d13df60e62c1
root <root@Do0ola.localdomain>
2023-04-29 12:49:18 +0200

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,6 +12,10 @@ int *array_range(int min, int max)
 	int arrlen, i, iterator;
 	int *ptr;
 
+	if (min >= max)
+	{
+		return (NULL);
+	}
 	arrlen = max - min + 1;
 	ptr = malloc(arrlen * sizeof(int));
 	if (!ptr)
