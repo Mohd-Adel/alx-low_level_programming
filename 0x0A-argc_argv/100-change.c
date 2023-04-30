18c64e3a4e5df3bb31618702babc6c44cc4c3ca3
root <root@Do0ola.localdomain>
2023-04-30 03:33:47 +0200

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  *main - prints min number of coins.
+ *@argc: args number.
+ *@argv: args values.
+ *Return: change.
  */
-void main(int argc, int *argv)
-{
-	int input = argv;
-	int change = 0;
 
-	change = (input / 25) + ((input % 25) / 10) + (((input % 25) % 10) / 5) + ((((input % 25) % 10) % 5) / 2) + (((((input % 25) % 10) % 5) % 2) / 1);
+void main(int argc, char *argv[])
+{
+	int input, i;
+	int change = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	input = atoi(argv[1]);
+	if (input < 0)
+	{
+		printf("0");
+	}
+	for (i = 0; coins[i]; i++)
+	{
+		if (input % coins[i] == 0)
+		{
+			change += (input / coins[i]);
+			break;
+		}
+		else
+		{
+			change += (input / coins[i]);
+			input -= (input * coins[i]);
+		}
+	}
 	printf("%d\n", change);
 }
