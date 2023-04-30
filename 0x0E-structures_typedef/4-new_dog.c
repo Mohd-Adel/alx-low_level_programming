82b0b0f67dc56864af8e99c1cda0a938f73701e1
root <root@Do0ola.localdomain>
2023-04-30 05:01:56 +0200

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,31 +14,14 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
-	int i;
-	char *namecpy, *ownercpy;
 
-	namecpy = malloc(strlen(name));
-	ownercpy = malloc(strlen(owner));
 	newdog = malloc(sizeof(dog_t));
-	newdog->name = malloc(sizeof(char) * strlen(name));
-	newdog->owner = malloc(sizeof(char) * strlen(owner));
-	if (!newdog->owner || !newdog->name || !newdog)
+	if(!newdog)
 	{
-		free(newdog->owner);
-		free(newdog->name);
-		free(newdog);
 		return (NULL);
 	}
-	for (i = 0; name[i]; i++)
-	{
-		newdog->name[i] = name[i];
-		namecpy[i] = name[i];
-	}
-	for (i = 0; owner[i]; i++)
-	{
-		newdog->owner[i] = owner[i];
-		ownercpy[i] = owner[i];
-	}
+	newdog->name = name;
+	newdog->owner = owner;
 	newdog->age = age;
 	return (newdog);
 }
