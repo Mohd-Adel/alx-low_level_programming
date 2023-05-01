9ae78d03ce1ad9fee9be77475dfcaed3c465f12d
root <root@Do0ola.localdomain>
2023-05-01 15:00:34 +0200

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,10 +19,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	i = 0;
 	if (*head && head)
 	{
-	while (*head && head && i < idx)
+	while (i < idx)
 	{
-		if (i < idx)
-		{
+		if ((*head)->next)
 		*head = (*head)->next;
 		i++;
 		}
