bf4b311a80066b1ec8f7d80c95023dcbccb3ec19
root <root@Do0ola.localdomain>
2023-05-01 13:12:14 +0200

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -15,7 +15,7 @@ void free_listint2(listint_t **head)
 	if (head)
 	{
 	currentitem = *head;
-	while(currentitem)
+	while (currentitem)
 	{
 		temp = currentitem;
 		currentitem = currentitem->next;
