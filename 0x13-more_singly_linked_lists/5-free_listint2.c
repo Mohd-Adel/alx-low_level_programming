29b4725b8c3c6769573a9d3b0ec9eb460c082d4d
root <root@Do0ola.localdomain>
2023-05-01 12:42:39 +0200

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head: input head.
+ * Return: free list.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *currentitem;
+
+	while((currentitem = *head))
+	{
+		currentitem = (*head)->next;
+		free(*head);
+		if (currentitem)
+		{
+		*head = currentitem;
+		}
+		else
+		{
+			*head = NULL;
+		}
+	}
+}
