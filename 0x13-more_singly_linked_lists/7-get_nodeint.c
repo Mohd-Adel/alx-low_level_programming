4770e4b75461e580756e82d75e8acd1e420ef434
root <root@Do0ola.localdomain>
2023-05-01 14:16:22 +0200

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,10 +13,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *item;
 
+	item = NULL;
+	if (head)
+	{
 	for (i = 0; i <= index; i++)
 	{
 		item = head;
 		head = head->next;
+	}
 	}
 
 	return (item);
