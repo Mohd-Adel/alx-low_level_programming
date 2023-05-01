4a1c33cb412a3d93eccdb3b1a6f363835e741260
root <root@Do0ola.localdomain>
2023-05-01 16:31:27 +0200

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new)
 		return (NULL);
 	iterator = *head;
-	if (!iterator)
+	if (!iterator && idx == 0)
 		return (NULL);
 	for (i = 0; i < idx && iterator; i++)
 	{
