140279e67b16b871ccf27f4c84f7a86058377d7e
root <root@Do0ola.localdomain>
2023-05-01 16:27:48 +0200

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
+	if (!head)
+		return (NULL);
 	iterator = *head;
 	for (i = 0; i < idx && iterator; i++)
 	{
