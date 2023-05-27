9017f91831be05766e7b4a59e9787b785d242620
Mohamed Ismael <mohamed.adel.ism@outlook.com>
2023-05-27 16:33:17 +0200

    task 3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+char *new_buff(char *file);
+void cl_file(int fd);
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
