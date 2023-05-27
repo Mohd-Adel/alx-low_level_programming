ddbf1300c30d24aa62d5ddd6bc7cc981b38f0bb0
Mohamed Ismael <mohamed.adel.ism@outlook.com>
2023-05-27 17:36:48 +0200

    updated 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - return counted numbers
+ * @n: input number
+ * @m: desired output
+ *
+ * Return: requred numbers
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int _xor = n ^ m, count = 0;
+
+	while (xor > 0)
+	{
+		count += (_xor & 1);
+		_xor >>= 1;
+	}
+
+	return (count);
+}
